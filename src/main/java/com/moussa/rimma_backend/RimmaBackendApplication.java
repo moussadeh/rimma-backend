bff7cc22933a78cd46809c97e2d234b4b95bdb33
moussa <moussadeh92@gmail.com>
2026-02-10 12:46:33 +0100

    start

---
+++ src/main/java/com/moussa/rimma_backend/RimmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moussa.rimma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimmaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moussa/rimma_backend/RimmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moussa.rimma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RimmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
