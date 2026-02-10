2d525b7e34cec52fc85fd4196244976cd1111fe9
moussa <moussadeh92@gmail.com>
2026-02-10 19:07:41 +0100

    dockerising

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
+FROM maven:3.9.6-eclipse-temurin-17 AS build
+
+WORKDIR /build
+
+COPY pom.xml .
+COPY src src
+
+RUN mvn clean package -DskipTests
+
+
 FROM eclipse-temurin:17
 
 WORKDIR /app
 
-COPY target/rimma-0.0.1.jar /app/rimma-0.0.1.jar
+COPY --from=build /build/target/*.jar /app/app.jar
 
 EXPOSE 9000
 
-CMD ["java", "-jar", "/app/rimma-0.0.1.jar"]
+CMD ["java", "-jar", "/app/app.jar"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- Vider la table avant insertion
---DELETE FROM utilisateurs;
+DELETE FROM utilisateurs;
 
 -- Réinitialiser l'auto-increment
---ALTER TABLE utilisateurs AUTO_INCREMENT = 1;
+ALTER TABLE utilisateurs AUTO_INCREMENT = 1;
 
---INSERT INTO utilisateurs (nom, prenom, email, telephone, mot_de_passe, role, active) VALUES
-    --('admin', 'admin', 'admin@admin.com', '770000001', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
-    --('client', 'client', 'client@client.com', '770000002', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
-    --('annonceur', 'annonceur', 'annonceur@annonceur.com', '770000003', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ANNONCEUR', true);
+INSERT INTO utilisateurs (nom, prenom, email, telephone, mot_de_passe, role, active) VALUES
+    ('admin', 'admin', 'admin@admin.com', '770000001', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
+    ('client', 'client', 'client@client.com', '770000002', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
+    ('annonceur', 'annonceur', 'annonceur@annonceur.com', '770000003', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ANNONCEUR', true);
