6ec6060eb65ab78fa7499f036d8337e10e739bc4
moussa <moussadeh92@gmail.com>
2026-02-14 22:41:09 +0100

    default data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
---TRUNCATE TABLE utilisateurs RESTART IDENTITY CASCADE;
+TRUNCATE TABLE utilisateurs RESTART IDENTITY CASCADE;
 
---INSERT INTO utilisateurs (nom, prenom, email, telephone, mot_de_passe, role, active) VALUES
-    --('admin', 'admin', 'admin@admin.com', '770000001', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
-    --('client', 'client', 'client@client.com', '770000002', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true);
+INSERT INTO utilisateurs (nom, prenom, email, telephone, mot_de_passe, role, active) VALUES
+    ('admin', 'admin', 'admin@admin.com', '770000001', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
+    ('client', 'client', 'client@client.com', '770000002', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true);
