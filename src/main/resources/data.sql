5dbae264a0f1dd53d07d9556acc24767ff4e1d49
moussa <moussadeh92@gmail.com>
2026-03-15 22:20:19 +0100

    données initial et vide bdd...

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ TRUNCATE TABLE utilisateurs RESTART IDENTITY CASCADE;
 INSERT INTO utilisateurs (nom, prenom, email, telephone, mot_de_passe, role, active) VALUES
     ('moussa', 'moussa', 'moussa@moussa.com', '000000000', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
     ('ib', 'ib', 'ib@ib.com', '000000000', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
-    ('atf', 'atf', 'atf@atf.com', '00000000', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true),
+    ('atf', 'atf', 'atf@atf.com', '00000000', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_ADMIN', true)
     --('client', 'client', 'client@client.com', '770000002', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
     --('client1', 'client1', 'client1@client1.com', '770000004', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
     --('client2', 'client2', 'client2@client2.com', '770000005', '$2a$12$B96IdtAD8Th5FvTPRoMKruUHqAU0DIqPLsS/R4yitrg911xD4xBNa', 'ROLE_CLIENT', true),
